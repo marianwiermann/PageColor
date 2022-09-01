412fb81be61579888713fa8f6646c0133fb23968
Márian Wiermann <mwcasas@hotmail.com>
2022-09-01 08:05:06 -0300

    primeiro tes te pink mode

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.btn').addEventListener('click', () => {
+  document.body.classList.toggle('pink-mode')
+})
+
+document.querySelector('.btn').addEventListener('click', () => {
+  document.body.classList.toggle('orange-mode')
+})
+
+document.querySelector('.btn').addEventListener('click', () => {
+  document.body.classList.toggle('green-mode')
+})
