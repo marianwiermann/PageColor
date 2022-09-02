71031723026907bf418aebe921fe550b0b5978ea
Márian Wiermann <mwcasas@hotmail.com>
2022-09-02 09:52:11 -0300

    pink-mode ok

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-document.querySelector('.btn').addEventListener('click', () => {
+document.querySelector('#btn-pink').addEventListener('click', () => {
   document.body.classList.toggle('pink-mode')
 })
 
-document.querySelector('.btn').addEventListener('click', () => {
+document.querySelector('#btn-orange').addEventListener('click', () => {
   document.body.classList.toggle('orange-mode')
 })
 
-document.querySelector('.btn').addEventListener('click', () => {
+document.querySelector('##btn-green').addEventListener('click', () => {
   document.body.classList.toggle('green-mode')
 })
